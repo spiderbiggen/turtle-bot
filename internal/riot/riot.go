dc88ca0c9627196792a6525512842c18a1e5babf
Stefan Breetveld <spiderbiggen@gmail.com>
2022-08-16 00:09:49 +0200

    add rate limiting to riot api

--- internal/riot/riot.go
+++ internal/riot/riot.go
@@ -4,26 +4,29 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"golang.org/x/time/rate"
 	"net/http"
+	"time"
+	"weeb_bot/internal/limiter"
 )
 
 var (
-	ErrInvalidStatus     = errors.New("invalid status code")
-	ErrRateLimitExceeded = errors.New("rate limit exceeded")
+	ErrInvalidStatus = errors.New("invalid status code")
 )
 
 type Client struct {
-	ApiKey string
-	Http   *http.Client
-	Rate   *rate.Limiter
+	ApiKey  string
+	Http    *http.Client
+	Limiter limiter.Limiter
 }
 
 func New(apiKey string) *Client {
 	return &Client{
 		ApiKey: apiKey,
 		Http:   http.DefaultClient,
-		Rate:   rate.NewLimiter(rate.Limit(20), 20),
+		Limiter: limiter.NewIntervalWindow(
+			limiter.Limit{Count: 20, Interval: 1 * time.Second},
+			limiter.Limit{Count: 100, Interval: 2 * time.Minute},
+		),
 	}
 }
 
@@ -31,10 +34,11 @@ func (c *Client) request(ctx context.Context, url string) (*http.Response, error
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 
 	req.Header.Set("X-Riot-Token", c.ApiKey)
-	err = c.Rate.Wait(ctx)
+	inc, err := c.Limiter.Wait(ctx)
 	if err != nil {
-		return nil, ErrRateLimitExceeded
+		return nil, err
 	}
+	defer inc()
 	resp, err := c.Http.Do(req)
 	if err != nil {
 		return nil, err
