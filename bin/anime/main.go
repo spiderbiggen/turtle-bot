fc557c00c423efb4158d9c14317f8f524675a845
Stefan Breetveld <spiderbiggen@gmail.com>
2023-01-30 22:10:10 +0100

    use anime-service

--- bin/anime/main.go
+++ bin/anime/main.go
@@ -7,8 +7,8 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"time"
+	animeApi "turtle-bot/internal/anime"
 	kitsuApi "turtle-bot/internal/kitsu"
-	nyaaApi "turtle-bot/internal/nyaa"
 	"turtle-bot/internal/storage/postgres"
 	"turtle-bot/internal/worker"
 )
@@ -17,13 +17,13 @@ func main() {
 	log.SetLevel(log.TraceLevel)
 	db := postgres.New()
 	kitsu := kitsuApi.New()
-	nyaa := nyaaApi.New()
+	anime := animeApi.New()
 
 	d, err := discordgo.New(fmt.Sprintf("Bot %s", os.Getenv("TOKEN")))
 	if err != nil {
 		log.Fatal(err)
 	}
-	w := worker.NyaaCheck(db, kitsu, nyaa, time.Now().Add(-24*time.Hour))
+	w := worker.NyaaCheck(db, kitsu, anime, time.Now().Add(-24*time.Hour))
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 	w(ctx, d)
